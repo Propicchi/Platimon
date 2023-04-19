c45734482e2ff541567bb6a8603c0748cdce7e2c
Mahmud Milon <propicchi@gmail.com>
2023-04-20 05:40:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function back(){
+  gamebox.style.display = 'block';
+}
+
+function g1fram(){
+  gamebox.style.display = 'none';
+  g1frame.style.display = 'block';
+}
+
+function g2fram() {
+  gamebox.style.display = 'none';
+  g2frame.style.display = 'block';
+}
+function g3fram() {
+  gamebox.style.display = 'none';
+  g3frame.style.display = 'block';
+}
+function g4fram() {
+  gamebox.style.display = 'none';
+  g4frame.style.display = 'block';
+}
+function g5fram() {
+  gamebox.style.display = 'none';
+  g5frame.style.display = 'block';
+}
+function g1fram() {
+  gamebox.style.display = 'none';
+  g1frame.style.display = 'block';
+}
+function g1fram() {
+  gamebox.style.display = 'none';
+  g1frame.style.display = 'block';
+}
+function upd(){
+  const answer = prompt('Wait Till Next Update ?');
+  if (answer === 'ok' || answer === 'Ok' || answer === '') {
+    alert('Thanks ♥️');
+  }
+  else if(answer === 'no' || answer === 'No'){
+    alert('Patience !!');
+  }
+}
